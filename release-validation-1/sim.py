dfb2d189a3f6415d4a185b8efbf242ff2f102684
tylern4 <tylern@canisius.edu>
2018-07-31 17:42:18 -0400

    Split input lund file based on number of cores

--- release-validation-1/sim.py
+++ release-validation-1/sim.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from multiprocessing import Pool, cpu_count
 import multiprocessing as mp
+from datetime import datetime
 import argparse
 import sys
 import glob
@@ -32,54 +33,83 @@ def tempdir():
         yield dirpath
 
 
-def make_list(args):
-    from datetime import datetime
+def make_names(output_dir, num):
     time = datetime.now().strftime('%m_%d_%Y-%H%M_')
     l = []
-    for i in range(0, args.num):
-        l.append(args.output + "sim_" + time + str(i))
+    for i in range(0, num):
+        l.append(output_dir + "sim_" + time + str(i))
     return l
+
+
+def split_lund(file_name, files):
+    import re
+    num_events = 0
+    total_lines = 0
+    with open(file_name, "r") as text_file:
+        lines = text_file.readlines()
+        for line in lines:
+            if line.startswith("          "):
+                num_events += 1
+
+        out = 0
+        tot = 0
+        f2 = open(files[out] + ".dat", "w")
+        for line_num, line in enumerate(lines):
+            if(tot >= num_events / len(files)):
+                tot = 0
+                if(f2):
+                    f2.close()
+                out += 1
+                if(out < len(files)):
+                    f2 = open(files[out] + ".dat", "w")
+                else:
+                    break
+
+            if line.startswith("          "):
+                tot += 1
+                num_e = int(re.findall(r'\d+', line)[0]) + 1
+                for x in range(num_e):
+                    f2.write(lines[line_num + x])
 
 
 def do_gemc(base):
     cwd = os.getcwd()
-    with tempdir() as dirpath:
-        with open(dirpath + "/do_sim.sh", "w") as text_file:
+    with tempdir() as dir_temp:
+        #shutil.copy(base + ".dat", dir_temp + "/input.dat")
+        shutil.copy(cwd+"/11gev_sidis_500.dat", dir_temp + "/input.dat")
+        with open(dir_temp + "/do_sim.sh", "w") as text_file:
             text_file.write(r"""#!/bin/bash
             source /jlab/2.2/ce/jlab.sh 2> /dev/null
-            /jlab/clas12Tags/4a.2.4/source/gemc clas12.gcard -USE_GUI=0 $@
+            /jlab/clas12Tags/4a.2.4/source/gemc clas12.gcard -USE_GUI=0 $@ -OUTPUT="evio, out.evio" -INPUT_GEN_FILE="LUND, input.dat"
             """)
 
         command = "docker run -v`pwd`:/jlab/workdir --rm -it jeffersonlab/clas12tags:4a.2.4 bash /jlab/workdir/do_sim.sh "
         out = " 1>" + base + ".out"
         err = " 2>" + base + ".err"
         command = command + err + out
-        os.system(command)
-        #shutil.copy(dirpath + "/out.evio", base + ".evio")
+        exit_code = os.system(command)
+        shutil.copy(dir_temp + "/out.evio", base + ".evio")
 
 
 def main():
     # Make argument parser
     parser = argparse.ArgumentParser(description="Full sim analysis")
     parser.add_argument('-c', dest='cores', type=int, nargs='?',
-                        help="Number of cores to use if not all the cores", default=0)
-    parser.add_argument('-n', dest='num', type=int, nargs='?',
-                        help="Number of simulations to do", default=100)
-    parser.add_argument('-o', dest='output', type=str, nargs='?',
-                        help="Output directory for final root files", default=os.getcwd())
-    parser.add_argument('-e', dest='events', type=int, nargs='?',
-                        help="Number of events to simulate", default=1000)
+                        help="Number of cores to use for simulation if not all the cores", default=0)
+    parser.add_argument('-o', dest='output_dir', type=str, nargs='?',
+                        help="Output directory for final files", default=os.getcwd())
+    parser.add_argument('-i', dest='events', type=str, nargs='?',
+                        help="Input event file to run gemc over", default="11gev_sidis_500.dat")
 
     args = parser.parse_args()
 
-    if args.output[-1] != '/':
-        args.output = args.output + '/'
-    if args.cores == 0 or cpu_count > cpu_count():
+    if args.output_dir[-1] != '/':
+        args.output_dir = args.output_dir + '/'
+    if args.cores == 0 or args.cores > cpu_count():
         args.cores = cpu_count()
-    if args.output[0] != '/':
-        args.output = os.getcwd() + args.output
 
-    files = make_list(args)
+    files = make_names(args.output_dir, args.cores)
+    #split_lund(args.events, files)
     pool = Pool(processes=args.cores)
     pool.imap_unordered(do_gemc, files)
     pool.close()
